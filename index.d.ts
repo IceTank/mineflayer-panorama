9db4e242b07b4e5115959cbc8282a38dc9d93c37
IceTank <niklaslehle@gmail.com>
2021-04-01 20:20:10 +0200

    -Small tweaks to camera.js and index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,13 @@ import { Bot } from 'mineflayer';
 export declare function plugin(bot: Bot): void;
 
 declare module 'panoramaImage' {
+    import ReadableStream = NodeJS.ReadableStream;
+
     export function image(bot: Bot): void;
 
-    export interface panoramaImage {
+    export interface moduleImage {
         takePicture(name: String): Promise<void>;
+        takePanoramaPictures(): Promise<ReadableStream>
     }
 }
 
