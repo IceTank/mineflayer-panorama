199889a3d39c725ea8a3a808df11c2b7f124c5ed
IceTank <niklaslehle@gmail.com>
2021-06-11 11:47:57 +0200

    Renamed and added more documentation

--- example/browserCubeMap/browserView.js
+++ example/browserCubeMap/browserView.js
@@ -1,15 +1,20 @@
+/**
+ * This example launches a website on port 8080 to display panorama images of the bots current location. 
+ * To launch this example run 'node browserView.js <host> [port] [email or name] [password]'.
+ */
+
 const mineflayer = require('mineflayer')
 const panorama = require('../../index')
 const server = require('../../lib/web')
 
-if (process.argv.length < 4 || process.argv.length > 6) {
-  console.log('Usage : node screenshot.js <host> <port> [<name>] [<password>]')
+if (process.argv.length < 3 || process.argv.length > 6) {
+  console.log('Usage : node browserView.js <host> [port] [email or name] [password]')
   process.exit(1)
 }
 
 const bot = mineflayer.createBot({
   host: process.argv[2],
-  port: parseInt(process.argv[3]),
+  port: process.argv[3] ? parseInt(process.argv[3]) : 25565,
   username: process.argv[4] ? process.argv[4] : 'screenshot',
   password: process.argv[5]
 })
@@ -20,10 +25,14 @@ bot.on('spawn', async () => {
   console.info('Bot spawned')
   bot.loadPlugin(panorama.image)
   if (!web.READY) {
-    await new Promise((resolve) => { web.on('ready', resolve) })
+    await new Promise((resolve) => { web.once('ready', resolve) })
   }
   await bot.waitForChunksToLoad()
   console.info('Ready to use')
+})
+
+bot.on('kicked', (reason) => {
+  console.info('Kicked for reason', reason)
 })
 
 bot.on('end', (reason) => {
