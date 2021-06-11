bc2280f9f18736a0fe2498c7b39f77ac4ef29609
IceTank <niklaslehle@gmail.com>
2021-06-11 11:03:34 +0200

    Made takePicture accessible

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const Camera = require(__dirname + '/lib/camera')
+const Camera = require('./lib/camera')
 
 function panoramaImage (bot) {
   bot.panoramaImage = {}
   const cam = new Camera(bot)
 
-  bot.panoramaImage.takePicture = async function (name) {
-    return cam.takePanoramaPictures(name)
-  }
+  bot.panoramaImage.takePicture = cam.takePicture
 
-  bot.panoramaImage.takePanoramaPictures = async function (name) {
-    return cam.takePanoramaPictures(name)
-  }
+  bot.panoramaImage.takePanoramaPictures = cam.takePanoramaPictures
 }
 
 module.exports = {
